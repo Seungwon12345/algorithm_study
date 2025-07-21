38dc92d96c322e6d9cded316fdc609b9226d94e2
Seungwon12345 <lesw04100@naver.com>
2025-07-21 12:01:49 +0900

    [Bronze II] Title: 저항, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,7 @@
+colors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+
+a = colors.index(input())
+b = colors.index(input())
+c = 10**colors.index(input())
+
+print(int(str(a) + str(b)) * c)
