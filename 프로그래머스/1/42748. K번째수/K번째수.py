21b5ca842cc5fd376520d30962ca2246a2114e1f
Seungwon12345 <lesw04100@naver.com>
2025-08-05 00:45:08 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.12 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+    for i in range(len(commands)):
+        n = commands[i][0]
+        j = commands[i][1]
+        k = commands[i][2]
+        
+        s_arr = sorted(array[n-1:j])
+        answer.append(s_arr[k-1])
+    return answer
